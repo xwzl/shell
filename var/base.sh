c9a5d8322494b893cfdb7943768f7739e6ce1b7e
徐伟智 <l>
2020-04-23 18:02:00 +0800

    主流程功能完善

--- var/base.sh
+++ var/base.sh
@@ -12,6 +12,8 @@ command_instead
 
 # 脚本参数的个数
 echo "$#"
+# 脚本参数内容列表
+echo "$@"
 
 # 脚本进程 id
 echo "$$"
@@ -19,6 +21,14 @@ echo "$$"
 # 上一次命令是否执行成功
 echo "$?"
 
+# 打印当前程序的名称
+echo "$0"
+
 # 过滤掉脚本本身进程 id
 grep -v $0
 
+# is_group_in_config 返回 0 或者 1, 0 执行 echo exist ，1 执行 echo not in exist
+#is_group_in_config $1 && echo exist || echo not in exist
+
+# 移除脚本输入时的第一个参数 -g demo => demo
+#shift
--- var/day-7/app_status.sh
+++ var/day-7/app_status.sh
@@ -5,7 +5,7 @@ CONFIG_FILE="process.cfg"
 # 脚本本身的进程 id，用于过滤脚本子进程 id
 this_pid="$$"
 
-echo "${HOME_DIR}"
+#echo "${HOME_DIR}"
 
 # Func:Get Process Status In process.cfg
 function get_all_group() {
@@ -41,6 +41,41 @@ function get_process_pid_by_name() {
 
 # 进程 id 获取对应的进程信息
 function get_process_info_by_pid() {
+  # awk 中不能直接外部参数，需要使用 -v 参数传递外部参数
+  # $1 代表函数传入的参数，$2 是 awk 处理的数据后锁返回的数据列
+  if [ $(ps -ef | awk -v pid=$1 '$2==pid{print }' | wc -l) -eq 1 ]; then
+    pro_status="RUNNING"
+  else
+    pro_status="STOPPED"
+  fi
+  # ps aux 中第二列为 pid,第三列为 cpu 使用率，第四列为 men 使用率
+  pro_cpu=$(ps aux | awk -v pid=$1 '$2==pid{print $3}')
+  pro_men=$(ps aux | awk -v pid=$1 '$2==pid{print $4}')
+  pro_start_time=$(ps -p $1 -o lstart | grep -v STARTED)
+  echo "${pro_status} ${pro_cpu} ${pro_men} ${pro_start_time}"
+}
+
+#
+function get_all_process_by_group() {
+  is_group_in_config $1
+  if [ $? -eq 0 ]; then
+    p_lists=$(sed -n "/\[$1\]/,/\[.*\]/p" "${HOME_DIR}/${CONFIG_FILE}" | egrep -v "(^$|^#|\[.*\])")
+    echo "$p_lists"
+  else
+    echo "Group name $1 is not in process.cfg"
+  fi
+
+}
+
+# 判断组是否在配置文件中
+function is_group_in_config() {
+  for gn in $(get_all_group); do
+    # 字符串需要用 == 比较，数字用 -eq
+    if [ "$gn" == "$1" ]; then
+      return
+    fi
+  done
+  return 1
 
 }
 
@@ -49,14 +84,39 @@ if [ ! -e "${HOME_DIR}/${CONFIG_FILE}" ]; then
   exit 1
 fi
 
-#p_list=$(get_all_process)
-#
-#echo "${p_list}"
+#get_all_group
 
-get_process_pid_by_name $1
+#get_all_process
 
-#for a in $(get_all_group); do
-#  echo "${a}"
-#done
-#g_list=$(get_all_group)
-#echo "${g_list}"
+#is_group_in_config $1 && echo exist || echo not in exist
+#get_all_process_by_group $1
+
+if [ $# -gt 0 ]; then
+  if [ "$1" == "-g" ]; then
+    # 移除脚本输入时的第一个参数 -g demo => demo
+    shift
+    # gn 代表的组名
+    for gn in $@; do
+      # 进程名
+      for pn in $(get_all_process_by_group $gn); do
+        #        pid=$(get_process_pid_by_name ${pn})
+        #        # 如果获取到进程信息
+        #        if [ "${pid}" != '' ]; then
+        #          get_process_info_by_pid ${pid}
+        #        else
+        #          echo "${pn} is stopped !"
+        #        fi
+        echo "待做"
+      done
+    done
+  else
+    # 参数不全，当做处理进程信息
+    for gn in $@; do
+      echo "待做"
+    done
+  fi
+else
+  for pg in $(get_all_process); do
+    echo "待做"
+  done
+fi
